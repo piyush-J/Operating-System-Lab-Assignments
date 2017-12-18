684235aa0f6435ee13cfa85c83a0fc81eebe80fb
Piyush Jha <piyushnit15@gmail.com>
2017-12-18 18:04:59 +0530

    Add files via upload

---
+++ lab-6-1a.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<unistd.h>
+int main(){
+int a;
+a=fork();
+if(!a){
+printf("My id is %u myparent id %u\n",getpid(),getppid());
+}
+else if(a){
+printf("My id is %u my parent id  s %u\n",getpid(),getppid());
+}
+return 0;
+}
