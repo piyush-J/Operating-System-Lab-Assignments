75d22fa90261e2ad035db1f81db7d75cb6ef6052
Piyush Jha <piyushnit15@gmail.com>
2017-12-18 18:05:34 +0530

    Add files via upload

---
+++ lab-6-2a.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<unistd.h>
+int main(){
+int ppid,pid;
+ppid=getppid();
+pid=fork();
+printf("Parent says -my id %d, my child id %u my parent id %u \n",getpid(),pid,getppid());
+printf("Child says - my id %u and my parent id %u my grandparent id %u\n",getpid(),getppid(),ppid);
+
+return 0;
+}
